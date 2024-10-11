bd77815111749e699d5feada69d4214316c63790
Katherine Hooper <Katherine.Hooper@dvla.gov.uk>
2024-10-11 20:07:07 +0100

    wip: outputting as hash to write to file

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,14 +6,25 @@ require_relative 'reg_generation'
 class Vehicle
   include RegGeneration
 
-  attr_reader :vrn, :make, :colour, :date_of_manufacture, :vin, :registration_area
+  attr_reader :make, :colour, :date_of_manufacture, :vin, :registration_area, :vrn
 
   def initialize(record)
-    @vrn = generate_vrn(record)
     @make = record[:make]
     @colour = record[:colour]
     @date_of_manufacture = record[:date_of_manufacture]
     @vin = record[:vin]
     @registration_area = record[:registration_area]
+    @vrn = generate_vrn(record)
+  end
+
+  def as_json
+    {
+      make: @make,
+      colour: @colour,
+      date_of_manufacture: @date_of_manufacture,
+      vin: @vin,
+      registration_area: @registration_area,
+      vrn: @vrn
+    }
   end
 end
